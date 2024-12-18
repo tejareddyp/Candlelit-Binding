e15123bbfa73954d8e3f613c6fe6c6db6b5ad5f9
teja <tejaswini.reddy@gmail.com>
2024-12-18 10:48:04 -0600

    Start of case study project

---
+++ src/main/java/com/casestudy/webapp/CandlelitBindingApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlelitBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandlelitBindingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casestudy/webapp/CandlelitBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandlelitBindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
